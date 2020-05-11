d40176081be0a746af287ae2eeac0783f149e768
Carlos Becerra <carlos.becerra1234@gmail.com>
2020-05-10 23:25:21 -0500

    Initial commit

---
+++ src/main/java/com/wedevol/springjavascheduler/SpringjavaschedulerApplication.java
@@ -0,0 +1,13 @@
+package com.wedevol.springjavascheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjavaschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjavaschedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wedevol/springjavascheduler/SpringjavaschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedevol.springjavascheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjavaschedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
